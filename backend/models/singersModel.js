4355fbadc204a6a385787cb8ccced9792e3d24eb
ellisvelandia <eyis619@gmail.com>
2022-10-25 21:09:21 -0500

    endpoints

--- backend/models/singersModel.js
+++ backend/models/singersModel.js
@@ -4,7 +4,7 @@ const singerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   description: { type: String, required: true },
-  gender: { type: String, required: true },
+  Genre: { type: String, required: true },
 });
 
 module.exports = new mongoose.model("singers", singerSchema);
--- backend/routes/singersRoutes.js
+++ backend/routes/singersRoutes.js
@@ -12,4 +12,61 @@ router.post("/add", async (req, res) => {
   }
 });
 
+// GET
+
+router.get("/getSingers", async (req, res) => {
+  let singers;
+  try {
+    singers = await singerModel.find();
+    res.status(200).json({ singers });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//GET REQ WITH  ID
+router.get("/getSingers/:id", async (req, res) => {
+  let singer;
+  const id = req.params.id;
+  try {
+    singer = await singerModel.findById(id);
+    res.status(200).json({ singer });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//UPDATE SINGERS BY ID
+
+router.put("/updateSingers/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, image, description, Genre } = req.body;
+  let singer;
+  try {
+    singer = await singerModel.findByIdAndUpdate(id, {
+      name,
+      image,
+      description,
+      Genre,
+    });
+    singer = await singer
+      .save()
+      .then(() => res.json({ message: "Data Update" }));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Delete Book BY id
+router.delete("/deleteSinger/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await singerModel
+      .findByIdAndDelete(id)
+      .then(() => res.status(201).json({ message: "Delete Successfully" }));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
